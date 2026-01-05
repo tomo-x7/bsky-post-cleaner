e489efbda8a191ea435d744f9866808c5c367a9e
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2026-01-06 01:25:57 +0900

    dekita

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,100 @@
-import { useState } from 'react'
+import type { Agent } from "@atproto/api";
+import type { BrowserOAuthClient } from "@atproto/oauth-client-browser";
+import { AtUri } from "@atproto/syntax";
+import { type PropsWithChildren, useState } from "react";
+import { toast } from "react-hot-toast";
 
-export function App() {
-  const [count, setCount] = useState(0)
+export function Layout({ children }: PropsWithChildren) {
+	return <div className="max-w-2xl mx-auto p-4">{children}</div>;
+}
 
-  return (
-    <>a
-    </>
-  )
+export function App({
+	client,
+	agent,
+	handle,
+}: {
+	client: BrowserOAuthClient;
+	agent: Agent | null;
+	handle: string | undefined;
+}) {
+	if (agent == null || handle == null) {
+		return <Signin client={client} />;
+	}
+	return <Main agent={agent} handle={handle} />;
+}
+
+function Signin({ client }: { client: BrowserOAuthClient }) {
+	const [handle, setHandle] = useState("");
+	const handleLogin = async () => {
+		if (!handle) return;
+		try {
+			await toast.promise(client.signIn(handle), { loading: "Signing in..." });
+		} catch (e) {
+			toast.error("Signin failed");
+		}
+	};
+	return (
+		<div>
+			<input type="text" value={handle} onChange={(e) => setHandle(e.target.value)} />
+		</div>
+	);
+}
+
+function Main({ agent, handle: usrHandle }: { agent: Agent; handle: string | undefined }) {
+	const [url, setUrl] = useState("");
+	const [loading, setLoading] = useState(false);
+	const handleClear = async () => {
+		try {
+			setLoading(true);
+			const parsed = parseUri(url);
+			if (parsed == null) return;
+			const { id, rkey } = parsed;
+			if (id !== usrHandle && id !== agent.assertDid) return void toast.error("自分以外のポストには使えません");
+
+			const post = await agent.app.bsky.feed
+				.getPosts({
+					uris: [`at://${agent.assertDid}/app.bsky.feed.post/${rkey}`],
+				})
+				.then((r) => r.data.posts[0]);
+			if (post == null) return void toast.error("ポストが見つかりません");
+
+			await agent.com.atproto.repo.putRecord({
+				repo: agent.assertDid,
+				collection: "app.bsky.feed.post",
+				rkey: rkey,
+				record: {
+					text: "bsky-post-cleanerにより削除中",
+					via: "bsky-post-cleaner",
+					createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24).toISOString(),
+				},
+			});
+			await agent.com.atproto.repo.deleteRecord({
+				repo: agent.assertDid,
+				collection: "app.bsky.feed.post",
+				rkey: rkey,
+			});
+		} catch (e) {
+			console.error(e);
+			toast.error("エラーが発生しました");
+		} finally {
+			setLoading(false);
+		}
+	};
+	return <div>Main</div>;
+}
+
+function parseUri(uri: string) {
+	try {
+		const aturi = new AtUri(uri);
+		if (aturi.collection !== "app.bsky.feed.post" || aturi.rkey == null)
+			return void toast.error("ポストのURIを入力してください");
+		return { id: aturi.hostname, rkey: aturi.rkey };
+	} catch {}
+	if (!URL.canParse(uri)) return void toast.error("無効なURL");
+	const parsed = new URL(uri);
+	if (parsed.hostname !== "bsky.app") return void toast.error("bsky.appのURLを入力してください");
+	const [_profile, handle, _post, rkey] = parsed.pathname.split("/");
+	if (_profile !== "profile" || !handle || _post !== "post" || !rkey)
+		return void toast.error("ポストのURLを入力してください");
+	return { id: handle, rkey };
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {App} from './App.tsx'
+import { Agent } from "@atproto/api";
+import { BrowserOAuthClient, type ClientMetadata } from "@atproto/oauth-client-browser";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import clientMetadata from "../public/client-metadata.json";
+import { App, Layout } from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const client = new BrowserOAuthClient({ clientMetadata: clientMetadata as unknown as ClientMetadata });
+const res = await client.init().catch((e) => {
+	console.error(e);
+	window.alert("初期化に失敗しました");
+});
+let agent: Agent | null;
+let handle: string | undefined;
+if (res?.session) {
+	agent = new Agent(res.session);
+	handle = await agent
+		.getProfile({ actor: agent.assertDid })
+		.then((r) => r.data.handle)
+		.catch((e) => {
+			console.error(e);
+			window.alert("プロフィールの取得に失敗しました");
+			agent = null;
+			return undefined;
+		});
+} else {
+	agent = null;
+}
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<Layout>
+			<App client={client} agent={agent} handle={handle} />
+			<Toaster position="top-right" />
+		</Layout>
+	</StrictMode>,
+);
